e76800a65ed619205761f511e7b9caeebc059a3c
divyanhjuyal07-lgtm <divyanhjuyal07@gmail.com>
2026-03-02 22:44:30 +0530

    UC5-Array initialisation

--- src/Uc1.java
+++ src/Uc1.java
@@ -3,22 +3,19 @@ public class StepClass {
         printBanner();
     }
 
-    /**
-     * Prints OOPS banner using concise array initialization
-     */
     public static void printBanner() {
 
-        String[] banner = new String[5];
-
-        banner[0] = String.join("  ", "OOOO", "OOOO", "PPPP", "SSSS");
-        banner[1] = String.join("  ", "O  O", "O  O", "P   P", "S");
-        banner[2] = String.join("  ", "O  O", "O  O", "PPPP", "SSSS");
-        banner[3] = String.join("  ", "O  O", "O  O", "P", "    S");
-        banner[4] = String.join("  ", "OOOO", "OOOO", "P", "SSSS");
+        // Store all banner lines inside an array
+        String[] banner = {
+                String.join("  ", "OOOO", "OOOO", "PPPP", "SSSS"),
+                String.join("  ", "O  O", "O  O", "P   P", "S"),
+                String.join("  ", "O  O", "O  O", "PPPP", "SSSS"),
+                String.join("  ", "O  O", "O  O", "P", "    S"),
+                String.join("  ", "OOOO", "OOOO", "P", "SSSS")
+        };
 
         for (String line : banner) {
             System.out.println(line);
         }
-
     }
 }
