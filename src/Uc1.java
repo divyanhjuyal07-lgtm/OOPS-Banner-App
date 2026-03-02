9055fd3eb502f6da5cb770e44c46bec8b34c5f4c
divyanhjuyal07-lgtm <divyanhjuyal07@gmail.com>
2026-03-02 22:36:14 +0530

    UC2-Banner

--- src/Uc1.java
+++ src/Uc1.java
@@ -1,6 +1,15 @@
-public class Uc1 {
-    static void main()
-    {
+public class main {
+    public static void main(String[] args) {
+
         System.out.println("OOPS");
+
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * ******* ******");
+        System.out.println("*     * *     * *            *");
+        System.out.println("*     * *     * *            *");
+        System.out.println(" *****   *****  *        *****");
     }
 }
