07ee1edd13ffaf1f175a99eefe1112529f8f8eb0
divyanhjuyal07-lgtm <divyanhjuyal07@gmail.com>
2026-03-02 22:46:22 +0530

    UC6-static branch

--- src/Uc1.java
+++ src/Uc1.java
@@ -5,17 +5,49 @@ public class StepClass {
 
     public static void printBanner() {
 
-        // Store all banner lines inside an array
-        String[] banner = {
-                String.join("  ", "OOOO", "OOOO", "PPPP", "SSSS"),
-                String.join("  ", "O  O", "O  O", "P   P", "S"),
-                String.join("  ", "O  O", "O  O", "PPPP", "SSSS"),
-                String.join("  ", "O  O", "O  O", "P", "    S"),
-                String.join("  ", "OOOO", "OOOO", "P", "SSSS")
-        };
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+
+        String[] banner = new String[o.length];
+
+        for (int i = 0; i < o.length; i++) {
+            banner[i] = String.join("  ", o[i], o[i], p[i], s[i]);
+        }
+
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    public static String[] buildO() {
+        return new String[]{
+                "OOOO",
+                "O  O",
+                "O  O",
+                "O  O",
+                "OOOO"
+        };
+    }
+
+    public static String[] buildP() {
+        return new String[]{
+                "PPPP",
+                "P   P",
+                "PPPP",
+                "P",
+                "P"
+        };
+    }
+
+    public static String[] buildS() {
+        return new String[]{
+                "SSSS",
+                "S",
+                "SSSS",
+                "    S",
+                "SSSS"
+        };
     }
 }
