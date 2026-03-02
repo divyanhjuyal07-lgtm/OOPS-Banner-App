cfb20a7cc14e01ba42f89b4b3e178f9bda27017a
divyanhjuyal07-lgtm <divyanhjuyal07@gmail.com>
2026-03-02 22:40:42 +0530

    UC3-String.join

--- src/Uc1.java
+++ src/Uc1.java
@@ -1,15 +1,49 @@
 public class StepClass {
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        printBanner();
+    }
 
+    /**
+     * Prints OOPS banner using String.join()
+     */
+    public static void printBanner() {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     ");
-        System.out.println("*     * *     * *     * *     ");
-        System.out.println("*     * *     * ******* ******");
-        System.out.println("*     * *     * *            *");
-        System.out.println("*     * *     * *            *");
-        System.out.println(" *****   *****  *        *****");
+        String line1 = String.join("  ",
+                "OOOO",
+                "OOOO",
+                "PPPP",
+                "SSSS");
+
+        String line2 = String.join("  ",
+                "O  O",
+                "O  O",
+                "P   P",
+                "S");
+
+        String line3 = String.join("  ",
+                "O  O",
+                "O  O",
+                "PPPP",
+                "SSSS");
+
+        String line4 = String.join("  ",
+                "O  O",
+                "O  O",
+                "P",
+                "    S");
+
+        String line5 = String.join("  ",
+                "OOOO",
+                "OOOO",
+                "P",
+                "SSSS");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+
     }
 }
